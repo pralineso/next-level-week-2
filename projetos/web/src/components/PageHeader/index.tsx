554d78991919357f5b8a8c30f9fc8b0c30fd9c19
Mariana <mxpixel@yahoo.com.br>
2020-08-07 17:49:01 -0300

    Aula 3 - Next Level Week Ed.2

--- projetos/web/src/components/PageHeader/index.tsx
+++ projetos/web/src/components/PageHeader/index.tsx
@@ -8,6 +8,7 @@ import './styles.css';
 
 interface PageHeaderProps {
     title: string;
+    description?: string;
 } /** para definir a propriedade, qnado colcoa a ? é não obrigatorio */
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
@@ -21,9 +22,8 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
             </div>
 
             <div className="header-content">
-                <strong>
-                    {props.title}
-                </strong>
+                <strong>{props.title}</strong>
+                {props.description && <p>{props.description}</p>}
 
                 
                 {props.children}
